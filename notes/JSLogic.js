aed0bdf93bade0edeafb6b42fe59a33a60a74d0f
flyweiing <flyweiing@163.com>
2018-08-20 00:37:10 +0800

    commit jslogic

--- notes/JSLogic.js
+++ notes/JSLogic.js
@@ -92,3 +92,72 @@ console.log(arr1) //["number", "undefined", "object", "object", "object", true, 
 // undefined
 // 2
 //--------------------------------------------------------------------------------------------------------------------------------
+
+
+setTimeout(function(){console.log(1)},0);
+new Promise(function(resolve,reject){
+   console.log(2);
+   resolve();
+}).then(function(){console.log(3)
+}).then(function(){console.log(4)});
+
+process.nextTick(function(){console.log(5)});
+
+console.log(6);
+//输出2,6,5,3,4,1
+//--------------------------------------------------------------------------------------------------------------------------------
+
+
+//深度克隆
+function deepClone(obj){
+    var result,oClass=isClass(obj);
+        //确定result的类型
+    if(oClass==="Object"){
+        result={};
+    }else if(oClass==="Array"){
+        result=[];
+    }else{
+        return obj;
+    }
+    for(key in obj){
+        var copy=obj[key];
+        if(isClass(copy)=="Object"){
+            result[key]=arguments.callee(copy);//递归调用
+        }else if(isClass(copy)=="Array"){
+            result[key]=arguments.callee(copy);
+        }else{
+            result[key]=obj[key];
+        }
+    }
+    return result;
+}
+//返回传递给他的任意对象的类
+function isClass(o){
+    if(o===null) return "Null";
+    if(o===undefined) return "Undefined";
+    return Object.prototype.toString.call(o).slice(8,-1);
+}
+var oPerson={
+    oName:"rookiebob",
+    oAge:"18",
+    oAddress:{
+        province:"beijing"
+    },    
+    ofavorite:[
+        "swimming",
+        {reading:"history book"}
+    ],
+    skill:function(){
+        console.log("bob is coding");
+    }
+};
+//深度克隆一个对象
+var oNew=deepClone(oPerson);
+ 
+oNew.ofavorite[1].reading="picture";
+console.log(oNew.ofavorite[1].reading);//picture
+console.log(oPerson.ofavorite[1].reading);//history book
+ 
+oNew.oAddress.province="shanghai";
+console.log(oPerson.oAddress.province);//beijing
+console.log(oNew.oAddress.province);//shanghai
